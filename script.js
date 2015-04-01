0c84537930fb5af6554e133e7048fb3355db4849
Mohit Kanwar <mkanwar@beachbody.com>
2015-04-01 21:54:41 +0530

    added automation

--- script.js
+++ script.js
@@ -9,11 +9,29 @@ if(!door.hasClass("disabled")){
 });
 function checkResults(){
 if(prizedDoor==getSelectedDoorNumber()){
-               alert("Congrats! You won");
+               $("#msgBox").text("Congrats! You won");
+               
         }
         else{
-                alert("Better luck next time!");
+                $("#msgBox").text("Better luck next time!");
         }
+        openAllDoors();
+        
+}
+function openAllDoors(){
+       var doorToBeOpened;
+        for (var i=1;i<=3;i++){
+                if(i==prizedDoor){
+                       var doorClass= getDoorClassFromNumber(i);
+                         $("."+doorClass).addClass("prize");
+                }
+                else{
+                var doorClass= getDoorClassFromNumber(i);
+                         $("."+doorClass).addClass("opened");
+                         $("."+doorClass).removeClass("selected");
+                }
+        }
+ 
 }
 function spoiler(){
 $("#spoiler").html( prizedDoor);
@@ -35,6 +53,20 @@ $("#description").append("Remember, the probability doesn't change on doors bein
         }
 $("#next").addClass("hidden");
 $("#checkResults").removeClass("hidden");
+$("#msgBox").html("Would you like to switch?<button onclick=\"switchDoor()\">switch</button>");
+}
+
+function switchDoor(){
+        for (var i=1;i<=3;i++){
+                var doorClass= getDoorClassFromNumber(i);
+              var door= $("."+doorClass);
+                if(!(isOpen(door)||isSelected(door))){
+                       
+                                selectDoor(door);
+                         
+                         
+                }
+        }
 }
 function openRandomUnselectedDoor(){
 /*False opens first one*/
@@ -77,13 +109,13 @@ function toggleSelectDoor(door){
 function selectDoor(door){
         
         $(".door").removeClass("selected");
-        $(".door").addClass("closed");
+        
         if(door.hasClass("closed")){
             door.addClass("selected");
-            door.removeClass("closed");
+            
             door.removeClass("opened");
          }
-         alert("The probability of prize being behind selected door is 1/3, and it being behind other doors combined is 2/3");
+         $("#msgBox").text("The probability of prize being behind selected door is 1/3, and it being behind other doors combined is 2/3");
 }
 
 function unselectDoor(door){
@@ -110,5 +142,40 @@ function getSelectedDoorNumber(){
            return 3;
        }
 }
+
+/**
+*   below is the automation part
+**/
+var selectedDoorNumberForAutomation;
+var switchforautomation=false;
+function automate(){
+var totalwins=0;
+        for (var i=1;i<=1000;i++){
+        setup();
+        switchrandomly();
+        checkResults();
+        var won=($("#msgBox").text()=="Congrats! You won");
+        if(won){
+        totalwins++;
+        }
+        $("#resulttable").append("<tr><td>"+i+"</td><td>"+selectedDoorNumberForAutomation+"</td><td>"+switchforautomation+"</td><td>"+prizedDoor+"</td><td>"+won+"</td></tr>");
+        }
+        $("#msgBox").text("you won "+totalwins+" times out of 1000");
+}
+function switchrandomly(){
+//don't switch
+}
+function setup(){
+$(".door").removeClass("opened");
+$(".door").removeClass("selected");
+$(".door").removeClass("prize");
+$(".door").addClass("closed"); 
+prizedDoor = Math.floor((Math.random() * 3) + 1);
+selectedDoorNumberForAutomation=Math.floor((Math.random() * 3) + 1);
+  var doorClass= getDoorClassFromNumber(selectedDoorNumberForAutomation);
+      var selecteddoor=  $("."+doorClass);
+        selectDoor(selecteddoor);
+}
+
 
 
